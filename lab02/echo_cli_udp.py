6eedd638bf41aad781b2b4d8eabac6849b304264
mikelmiras <mmiras004@ikasle.ehu.eus>
2023-09-22 13:11:51 +0200

    Cliente y servidor UDP con todas las mejoras

--- lab02/echo_cli_udp.py
+++ lab02/echo_cli_udp.py
@@ -5,28 +5,38 @@ import socket, sys
 PORT = 50007
 
 # Comprueba que se ha pasado un argumento.
-if len( sys.argv ) != 2:
-	print( "Uso: {} <servidor>".format( sys.argv[0] ) )
+if len( sys.argv ) == 1:
+	print( "Uso: {} <servidor> <puerto>".format( sys.argv[0] ) )
 	exit( 1 )
 
 """A COMPLETAR POR EL/LA ESTUDIANTE:
 Crear el socket.
 """
-
+try:
+	puerto = int(sys.argv[2])
+except:
+	puerto = int(input("¿Qué puerto utiliza el servidor UDP?\n"))
 UDP_SERVER = sys.argv[1]
 
+
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# print("Iniciado cliente UDP en {}:{}".format())
 print( "Introduce el mensaje que quieres enviar (mensaje vacío para terminar):" )
 while True:
 	mensaje = input()
 	if not mensaje:
 		s.close()
 		break
+	if len(mensaje.encode()) > 8:
+		print("No puedes enviar mensajes de más de 8 bytes")
+		continue
 	print("Enviando {} caracteres ({} bytes)".format(len(mensaje), len(mensaje.encode())))
-	s.sendto(mensaje.encode(), (UDP_SERVER, PORT))
+	s.sendto(mensaje.encode(), (UDP_SERVER, puerto))
+	print(s.getsockname())
 
 	resp = s.recv(1024)
-	print(resp.decode())
+	print("Respuesta del servidor: ", resp.decode())
 	"""A COMPLETAR POR EL/LA ESTUDIANTE:
 	Enviar mensaje y recibir 'eco'.
 	Mostrar en pantalla lo recibido.
--- lab02/echo_ser_udp.py
+++ lab02/echo_ser_udp.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
 import socket
-
+import random
 PORT = 50007
 
 """A COMPLETAR POR EL/LA ESTUDIANTE:
 Crear un socket y asignarle su direccion.
 """
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-s.bind(("", PORT))
+s.bind(("", 0))
+print("UDP server initialized at port {}".format(s.getsockname()[1]))
 while True:
 	"""A COMPLETAR POR EL/LA ESTUDIANTE:
 	Recibir un mensaje y responder con el mismo.
